d59b29e42106767f13b87bc450ba836fe795a984
galbi <galbi@ANA>
2023-03-06 17:34:36 +0200

    commit

---
+++ Seminarii/Seminar_2/cts/clase/reader/ReaderAplicant.java
@@ -0,0 +1,10 @@
+package cts.clase.reader;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import cts.clase.Aplicant;
+
+public abstract class ReaderAplicant {
+	public abstract List<Aplicant> readAplicants(String file) throws FileNotFoundException;
+}
